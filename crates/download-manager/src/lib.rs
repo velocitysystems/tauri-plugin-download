63daefcf2e891765c2f8aa65b52560ec08012ade
Matthew Richardson <matthew@velocityengineering.co.uk>
2026-08-28 14:20:18 +0100

    feat: allow configuring the download store location

--- crates/download-manager/src/lib.rs
+++ crates/download-manager/src/lib.rs
@@ -8,4 +8,5 @@ mod validate;
 pub use error::{Error, Result};
 pub use manager::{DownloadManager, DownloadManagerConfig, OnChanged};
 pub use models::{CreateOptions, DownloadActionResponse, DownloadItem, DownloadStatus};
+pub use validate::store_dir as validate_store_dir;
 pub use validate::user_agent as validate_user_agent;
